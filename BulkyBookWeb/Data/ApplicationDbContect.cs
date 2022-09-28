53078696fbd0b1332293337b2aef5f17cb2f0b23
Irfan <irfancesir93@gmail.com>
2022-09-29 01:10:25 +0200

    added first migration, updated database, created CategoryControlled and its view

--- BulkyBookWeb/Data/ApplicationDbContect.cs
+++ BulkyBookWeb/Data/ApplicationDbContect.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Data
 {
-    public class ApplicationDbContect : DbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
 
+        }
+        public DbSet<Category> Categories { get; set; }
     }
 }
