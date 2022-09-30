56a748eb26fac25ffce158596b2d2403f19ec51b
Irfan <irfancesir93@gmail.com>
2022-09-30 16:27:32 +0200

    created get and post method

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -18,5 +18,21 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category>objCategoriesList= _db.Categories;
             return View(objCategoriesList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
